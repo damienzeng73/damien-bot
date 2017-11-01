5ca02af703735cb4288451610d9d0abfebf4e5de
damnee562 <damnee562@gmail.com>
2017-11-01 16:39:42 +0800

    Get latest news from Apple Daily.

--- app.py
+++ app.py
@@ -34,11 +34,36 @@ def callback():
     return 'OK'
 
 
+def apple_news():
+    target_url = 'https://tw.appledaily.com/new/realtime'
+    req = requests.get(target_url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    content = ""
+
+    for index, data in enumerate(soup.select('.rtddt a'), 0):
+    if index == 10:
+        return content
+
+    url = data['href']
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    heading = soup.find('article', attrs={'class': 'ndArticle_leftColumn'}).find('h1').text
+    content += "{}\n{}\n".format(heading, data['href'])
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.message.text == 'Apple news':
+        content = apple_news()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 
 if __name__ == "__main__":
