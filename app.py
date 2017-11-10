2d0fc20202c8e534b3c24b640e5174f37dd1590b
damnee562 <damnee562@gmail.com>
2017-11-10 15:36:05 +0800

    Get latest articles from PTT Gossiping.

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ def callback():
 
 def apple_news():
     target_url = 'https://tw.appledaily.com/new/realtime'
-    req = requests.get(target_url)
-    res = BeautifulSoup(req.text, 'html.parser')
+    res = requests.get(target_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
     content = ""
 
-    for index, data in enumerate(res.select('.rtddt a'), 0):
+    for index, data in enumerate(soup.select('.rtddt a'), 0):
         if index == 10:
             return content
 
@@ -53,9 +53,9 @@ def apple_news():
 
 def yahoo_movies():
     target_url = 'https://tw.movies.yahoo.com/movie_thisweek.html'
-    req = requests.get(target_url)
-    res = BeautifulSoup(req.text, 'html.parser')
-    pages = len(res.select_one('.page_numbox').find_all('li')) - 4
+    res = requests.get(target_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    pages = len(soup.select_one('.page_numbox').find_all('li')) - 4
     content = ""
 
     for i in range(pages):
@@ -63,10 +63,10 @@ def yahoo_movies():
             pass
         else:
             target_url = 'https://tw.movies.yahoo.com/movie_thisweek.html?page={}'.format(str(i + 1))
-            req = requests.get(target_url)
-            res = BeautifulSoup(req.text, 'html.parser')
+            res = requests.get(target_url)
+            soup = BeautifulSoup(res.text, 'html.parser')
 
-        for data in res.select('.release_info_text'):
+        for data in soup.select('.release_info_text'):
             heading = data.find('div', attrs={'class': 'release_movie_name'}).find('a', attrs={'class': 'gabtn'}, href=True)
             name = heading.text.strip()
             link = heading['href']
@@ -78,12 +78,52 @@ def yahoo_movies():
     return content
 
 
+def crawl_ptt(res):
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+
+    for index, data in enumerate(soup.select('.r-ent'), 0):
+        pushes = data.select_one('.nrec').text
+        if pushes == '爆' or (pushes != '' and 'X' not in pushes and int(pushes) > 10):
+            title = data.find('a', href=True)
+            heading = title.text
+            link = 'https://www.ptt.cc' + title['href']
+
+            content += "{}\n{}\n\n".format(heading, link)
+
+    return content
+
+
+def ptt_gossiping():
+    rs = requests.session()
+    data = {
+        'from': '/bbs/Gossiping/index.html',
+        'yes': 'yes'
+    }
+
+    res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=data)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    last_page_url = 'https://www.ptt.cc' + soup.select('.btn.wide')[1]['href']
+    content = crawl_ptt(res)
+
+    while (len(content.split('https')) - 1) < 10:
+        current_page_index = last_page_url.split('index')[1].split('.html')[0]
+        last_page_url = 'https://www.ptt.cc' + '/bbs/Gossiping/index{}.html'.format(str(int(current_page_index) - 1))
+
+        res = rs.get(last_page_url, verify=False)
+        content += crawl_ptt(res)
+
+    return content
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == 'Apple news':
         content = apple_news()
     elif event.message.text == 'Yahoo movies':
         content = yahoo_movies()
+    elif event.message.text == 'PTT Gossiping':
+        content = ptt_gossiping()
     else:
         content = event.message.text
 
@@ -94,12 +134,16 @@ def handle_message(event):
                 text='請選擇',
                 actions=[
                     MessageTemplateAction(
-                        label='蘋果即時新聞',
+                        label='即時新聞',
                         text='Apple news'
                     ),
                     MessageTemplateAction(
-                        label='Yahoo奇摩電影',
+                        label='本週上映電影',
                         text='Yahoo movies'
+                    ),
+                    MessageTemplateAction(
+                        label='PTT八卦板大於10推文章',
+                        text='PTT Gossiping'
                     )
                 ]
             )
